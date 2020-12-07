3d9fd3b37d34c4f6745014cf308488a3bce5b9c6
iskewed <jtornello2019@hotmail.com>
2020-12-07 13:37:40 -0300

    Added CORS middleware to prevent cors blocking

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('./startup/routes')(app); //Routes settings
 require('./startup/config')(); //Environment configuration
 require('./startup/validation')(); //Api Validation initialization
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const server = app.listen(port, () => console.log(`Initializing on port ${port}...`));
 
 module.exports = server;
--- startup/routes.js
+++ startup/routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const usersRoute = require('../routes/users');
 const authRoute = require('../routes/auth');
 const listsRoute = require('../routes/lists');
+const cors = require('../middleware/cors');
 const { endpoints } = require('../config.json');
 
 module.exports = function (app) {
   app.use(express.json());
+  app.use(cors);
   app.use(endpoints.Users, usersRoute);
   app.use(endpoints.Auth, authRoute);
   app.use(endpoints.Lists, listsRoute);
